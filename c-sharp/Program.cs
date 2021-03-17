51909373016883442d5e294cb6cddce88db5d506
RedneyMonteiro15 <redneymonteiro15@gmail.com>
2021-03-17 23:21:20 +0000

    adicionando funcionalidades

--- c-sharp/Program.cs
+++ c-sharp/Program.cs
@@ -8,28 +8,36 @@ namespace c_sharp
     {
         static void Main(string[] args)
         {
-            int num, c = 0;
+            int num, c = 0, contS = 0, contN = 0;
             Random aleatorio = new Random();
             List<int> listaNum = new List<int>(){aleatorio.Next(0, 9), aleatorio.Next(0, 9), aleatorio.Next(0, 9), aleatorio.Next(0, 9), aleatorio.Next(0, 9)};
             foreach (var item in listaNum)
             {
                 Console.WriteLine(item);
                 Thread.Sleep(1000);
-                Console.Clear();
+                //Console.Clear();
             }
             foreach (var item in listaNum)
             {
-                Console.WriteLine($"Digite {c}º número: ");
+                Console.Write($"Digite {c+1}º número: ");
                 num = int.Parse(Console.ReadLine());
-                if(num == listaNum[0])
+                if(num == listaNum[c])
                 {
                     Console.ForegroundColor = ConsoleColor.Green;
                     Console.WriteLine("ACERTASTE!!!");
-                    Console.ResetColor();
+                    contS += 1;
                 }
+                else
+                {
+                    contN += 1;
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("ERRADO!!!");
+                }
+                Console.ResetColor();
                 c++;
             }
-
+            Console.WriteLine($"Acertaste {contS} números");
+            Console.WriteLine($"Erraste {contN} números");
         }
     }
 }
