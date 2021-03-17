9be0b994a97d7ab446b554c9ab2b9eba98559fe8
RedneyMonteiro15 <redneymonteiro15@gmail.com>
2021-03-17 23:07:39 +0000

    criando projeto

---
+++ c-sharp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+
+namespace c_sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, c = 0;
+            Random aleatorio = new Random();
+            List<int> listaNum = new List<int>(){aleatorio.Next(0, 9), aleatorio.Next(0, 9), aleatorio.Next(0, 9), aleatorio.Next(0, 9), aleatorio.Next(0, 9)};
+            foreach (var item in listaNum)
+            {
+                Console.WriteLine(item);
+                Thread.Sleep(1000);
+                Console.Clear();
+            }
+            foreach (var item in listaNum)
+            {
+                Console.WriteLine($"Digite {c}º número: ");
+                num = int.Parse(Console.ReadLine());
+                if(num == listaNum[0])
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("ACERTASTE!!!");
+                    Console.ResetColor();
+                }
+                c++;
+            }
+
+        }
+    }
+}
