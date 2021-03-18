419fe120b81a6d75476581de2274e11f3d0d5736
RedneyMonteiro15 <redneymonteiro15@gmail.com>
2021-03-18 14:19:17 +0000

    logica feita

--- c-sharp/Program.cs
+++ c-sharp/Program.cs
@@ -8,36 +8,74 @@ namespace c_sharp
     {
         static void Main(string[] args)
         {
-            int num, c = 0, contS = 0, contN = 0;
-            Random aleatorio = new Random();
-            List<int> listaNum = new List<int>(){aleatorio.Next(0, 9), aleatorio.Next(0, 9), aleatorio.Next(0, 9), aleatorio.Next(0, 9), aleatorio.Next(0, 9)};
-            foreach (var item in listaNum)
+            int num, i = 0, contS = 0, contN = 0, total = 0;
+            while (true)
             {
-                Console.WriteLine(item);
-                Thread.Sleep(1000);
-                //Console.Clear();
-            }
-            foreach (var item in listaNum)
-            {
-                Console.Write($"Digite {c+1}º número: ");
-                num = int.Parse(Console.ReadLine());
-                if(num == listaNum[c])
+                int c = 0;
+                Random aleatorio = new Random();
+                List<int> listaNum = new List<int>();
+                for(int d = 0; d < i+1; d++)
                 {
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine("ACERTASTE!!!");
-                    contS += 1;
+                    listaNum.Add(aleatorio.Next(0, 9));
+                }
+                Console.BackgroundColor = ConsoleColor.Blue;
+                Console.ForegroundColor = ConsoleColor.White;
+                foreach (var item in listaNum)
+                {
+                    Console.Write($" {item}");
+                    Thread.Sleep(1000);
+                }
+                Console.WriteLine(" ");
+                Console.ResetColor();
+                foreach (var item in listaNum)
+                {
+                    Console.Write($"Digite {c+1}º número: ");
+                    num = int.Parse(Console.ReadLine());
+                    if(num == listaNum[c])
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("ACERTASTE!!!");
+                        Console.ResetColor();
+                        contS += 1;
+                    }
+                    else
+                    {
+                        contN += 1;
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("GAME OVER!!!");
+                        Console.ResetColor();
+                        break;
+                    }
+                    c++;
+                }
+                if(contN == 1)
+                {
+                    break;
+                }
+                total++;
+                i++;
+            }
+            if(total == 0)
+            {
+                Console.WriteLine("Não Acertaste nenhuma vez!!!");
+            }
+            else if(total == 1)
+            {
+                Console.WriteLine($"Ganhaste apenas {total} partida!!!");
+                if(contS == 1)
+                {
+                    Console.WriteLine($"Acertaste {contS} vez!!!");
                 }
                 else
                 {
-                    contN += 1;
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("ERRADO!!!");
+                    Console.WriteLine($"Acertaste {contS} vezes!!!");
                 }
-                Console.ResetColor();
-                c++;
             }
-            Console.WriteLine($"Acertaste {contS} números");
-            Console.WriteLine($"Erraste {contN} números");
+            else
+            {
+                Console.WriteLine($"Ganhaste {total} partidas!!!");
+                Console.WriteLine($"Acertaste {contS} vezes");
+            }
         }
     }
 }
